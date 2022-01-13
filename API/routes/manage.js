84b329cfe4d5f9e190ed90fae6037201c075b193
siyabongagoqoza <66440094+siyabongagoqoza@users.noreply.github.com>
2022-01-13 15:23:17 +0200

    Add files via upload

--- API/routes/manage.js
+++ API/routes/manage.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
 const request = require('request')
 
@@ -7,7 +6,13 @@ router.get('/', (req, res) => {
     const url = `${process.env.WEATHER_URL + req.body.city}&appid=${process.env.WEATHER_API_KEY}`;
     
     request({url, json:true}, (error, data) => {
-        res.send(data);
+        // console.log(data.body)
+        res.send({
+            temperature: data.body.main.temp - 273.15,
+            description: data.body.weather[0].description,
+            city: data.body.name
+
+        });
     });
 });
 
