8262074f1d64a54486ea44d5befc8bd9f3ab1888
siyabongagoqoza <66440094+siyabongagoqoza@users.noreply.github.com>
2022-01-21 14:49:24 +0200

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Import pages
+import Display from './Pages/display_board'
+import Admin from './Pages/admin_board'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/' exact element={<Display />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
